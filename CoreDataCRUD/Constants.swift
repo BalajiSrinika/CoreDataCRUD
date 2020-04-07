59baaca8b892a517ad4bbe2b3e37fc1f267c986c
Balaji <pbalajieee@gmail.com>
2020-04-07 20:55:37 +0530

    initial commit

---
+++ CoreDataCRUD/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  CoreDataCRUD
+//
+//  Created by Sabari on 07/04/20.
+//  Copyright © 2020 Sabari. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+let appDelegate = UIApplication.shared.delegate as! AppDelegate
+
+let reachability = Reachability()
+
+/// Api
+let Api = "http://www.json-generator.com/api/json/get/cwsVHhOGeW?indent=2"
+     
